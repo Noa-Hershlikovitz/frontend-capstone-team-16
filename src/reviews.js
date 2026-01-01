526f090b94e8d5170f0941100d2f9c17dac7a1df
InbarEliya <inbar.e2000@gmail.com>
2026-01-01 16:10:43 +0200

    update

--- src/reviews.js
+++ src/reviews.js
@@ -1,15 +1,3 @@
-// ✨ חדש – תפיסת הטופס לפי class
-const form = document.querySelector(".testimonial-form");
-
-const banner = document.getElementById("success-banner");
-
-function showBanner() {
-  banner.style.display = "block";
-
-  setTimeout(() => {
-    banner.style.display = "none";
-  }, 3000);
-}
 
 // ✨ עובד עכשיו כי form מוגדר
 form.addEventListener("submit", function (e) {
