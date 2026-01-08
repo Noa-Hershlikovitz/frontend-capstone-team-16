b732b2eaab2e4e7e26233bdb8dfcadfc3e790ace
InbarEliya <inbar.e2000@gmail.com>
2026-01-08 17:38:15 +0200

    css

--- src/reviews.js
+++ src/reviews.js
@@ -1,7 +1,5 @@
-
-// ✨ עובד עכשיו כי form מוגדר
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   showBanner();
-  form.reset(); // ✨ אופציונלי – מנקה את הטופס
+  form.reset();
 });
